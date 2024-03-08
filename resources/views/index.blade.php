3baf1461ab9d1f3268454e919878c5834cc9c8d9
Isalanctos <101016039+SalesTheCoder@users.noreply.github.com>
2024-03-08 11:49:57 -0300

    arrumando index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,8 +2,8 @@
     <ul>
         <li><a href="/cadastrarpessoa">cadastrar pessoa</a></li>
         <li><a href="/cadastrarcidade">cadastrar cidade</a></li>
-        <li><a href="/consultarpessoa">consultar pessoa</a></li>
-        <li><a href="/consultarcidade">consultar cidade</a></li>
+        <li><a href="#">consultar pessoa</a></li>
+        <li><a href="#">consultar cidade</a></li>
         <li><a href="/relatorio">relatório</a></li>
     </ul>
     
